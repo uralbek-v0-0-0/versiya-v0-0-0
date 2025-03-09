a7e995b007c1a91de63ce169a13a10301b86c292
Uralbek Tursunov <tursunovuralbek000@gmail.com>
2025-03-09 22:59:14 +0500

    update index.js

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ document.getElementById("mobil_logotip").innerHTML = "Uralbek UZ";
 document.getElementById("medium_logotip").innerHTML = "Uralbek UZ";
 document.getElementById("desktop_logotip").innerHTML = "Uralbek UZ";
 
-// Display eslatmasi
-document.getElementById("mobil_eslatma").innerHTML = "Veb-sahifaning mobil versiyasidan foydalanyapsiz,<br>medium yoki desktop versiyani ko‘rish uchun ekran o‘lchamini o‘zgartiring!";
-document.getElementById("medium_eslatma").innerHTML = "Veb-sahifaning medium (planshet uchun) versiyasidan foydalanyapsiz,<br>mobil yoki desktop versiyani ko‘rish uchun ekran o‘lchamini o‘zgartiring!";
-document.getElementById("desktop_eslatma").innerHTML = "Veb-sahifaning desktop (katta ekranli) versiyasidan foydalanyapsiz,<br>mobil yoki medium versiyani ko‘rish uchun ekran o‘lchamini o‘zgartiring!";
+
 
 
 
