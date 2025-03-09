c2f71622962a25d3451c6dd5c698aabed30365b8
Uralbek Tursunov <tursunovuralbek000@gmail.com>
2025-03-09 20:05:38 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 document.getElementById("mobil_logotip").innerHTML = "Uralbek UZ";
+
+
+
+
+
+
+function ochish() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function yopish() {
+  document.getElementById("mySidebar").style.display = "none";
+}
