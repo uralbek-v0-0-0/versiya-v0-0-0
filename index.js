ac57ddb3998b3d9bf57cad8c76707272d74d700c
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-08-26 14:20:32 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-document.getElementById("image").innerHTML = "<img class='desktop_logo' src='uralbek.ico' alt='logotip' width='70px'>";
+document.getElementsByClassName("image").innerHTML = "<img class='desktop_logo' src='uralbek.ico' alt='logotip' width='70px'>";
 document.getElementById("astronom").innerHTML = "Astronomiya bu lotincha so‘z bo‘lib";
