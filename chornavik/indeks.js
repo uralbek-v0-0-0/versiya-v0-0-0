766e9c922f3645bf983a3e335618287eb11f6ed9
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-10-27 13:17:48 +0500

    update indeks.js

---
+++ chornavik/indeks.js
@@ -0,0 +1,2 @@
+document.getElementById("image").innerHTML = "<img class='desktop_logo' src='uralbek.ico' alt='logotip' width='70px'>";
+document.getElementById("astronom").innerHTML = "Astronomiya bu lotincha so‘z bo‘lib";
