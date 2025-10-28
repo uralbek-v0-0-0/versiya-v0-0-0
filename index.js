49588a5d1db2d9a0d5ca412b9c396b77d69a5608
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-10-28 20:39:01 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.getElementById("desktop").innerHTML = "<div id='ds_top'><div id='";
+document.getElementById("desktop").innerHTML = "<div id='ds_top'><div id='ds_header'>";
 document.getElementById("planshet").innerHTML = "";
 document.getElementById("mobil").innerHTML = "";
