1687d77d03cfcd1e86a7563ba3a2993888898f0a
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-10-28 22:02:55 +0500

    update indeks.js

--- chornavik/indeks.js
+++ chornavik/indeks.js
@@ -1,2 +1,9 @@
 document.getElementById("image").innerHTML = "<img class='desktop_logo' src='uralbek.ico' alt='logotip' width='70px'>";
 document.getElementById("astronom").innerHTML = "Astronomiya bu lotincha so‘z bo‘lib";
+
+document.getElementById("desktop").innerHTML = "<div id='ds_top'></div>";
+document.getElementById("planshet").innerHTML = "";
+document.getElementById("mobil").innerHTML = "";
+
+document.getElementById("ds_top").innerHTML = "<div id='top'><div id='header'><b id='logotip'>Uralbek UZ</b></div></div>";
+document.getElementById("makaron").innerHTML = "Media bo'limi tayyor bo'lishi bilan xabardor qilinasiz!";
