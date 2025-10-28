6243a71b74d2c8040c4675a836b87f2a7a3bb553
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-10-28 12:01:14 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.getElementById("desktop").innerHTML = "";
 document.getElementById("planshet").innerHTML = "";
-document.getElementById("mobil").innerHTML = "";
+document.getElementById("mobil").innerHTML = "<div id='top'><div id='header"><b id="logotip">Uralbek UZ</b></div></div>";
