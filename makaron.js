762d35b7a224d114c8edf854e0f9ab9f041c8e38
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2025-10-28 20:49:39 +0500

    update makaron.js

--- makaron.js
+++ makaron.js
@@ -1,2 +1,2 @@
-document.getElementById("makaron").innerHTML = "Media bo'limi tayyor bo'lishi bilan xabardor qilinasiz!";
 document.getElementById("ds_top").innerHTML = "<div id='top'><div id='header'><b id='logotip'>Uralbek UZ</b></div></div>";
+document.getElementById("makaron").innerHTML = "Media bo'limi tayyor bo'lishi bilan xabardor qilinasiz!";
