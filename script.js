8625f708b31f83c80b3af91543eccf52966c5423
Versiya v0.0.0 <tursunovuralbek000@gmail.com>
2026-02-12 19:13:11 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* ===== SOZLAMALAR ===== */
-const WEATHER_API_KEY = "YOUR_OPENWEATHERMAP_API_KEY";
+const WEATHER_API_KEY = "840a8ec0caec0104a8633e901bdd873b";
 const CITY = "Tashkent";
 const RATE_API = "https://api.exchangerate.host/latest?base=USD&symbols=UZS";
 
